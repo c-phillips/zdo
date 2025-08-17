49d9e94276e37d5e1826ee4f3f321d5c4c1cc9ff
Collin Phillips <collinp1024@gmail.com>
2025-08-17 12:19:06 -0400

    Simplified main file

--- src/main.zig
+++ src/main.zig
@@ -14,29 +14,24 @@ pub fn main() !void {
     const raw_args = try std.process.argsAlloc(alloc);
     defer std.process.argsFree(alloc, raw_args);
 
-    const parsed_args: ?Args = Args.init(alloc, raw_args) catch |err| blk: {
+    var args: Args = Args.init(alloc, raw_args) catch |err| {
         switch (err) {
             error.NoCommand => std.log.err("No command! Rerun with help for a list of commands", .{}),
             error.InvalidArgument => std.log.err("Invalid argument!", .{}),
             error.BadOption => std.log.err("Invalid option!", .{}),
             error.OutOfMemory => std.log.err("Out of Memory!", .{}),
         }
-        break :blk null;
+        return err;
     };
-    // TODO: We should probably return an error on failure since any external
-    //       program calling zdo will be on the lookout for a bad return code
 
-    if (parsed_args) |*argsptr| {
-        var args = argsptr.*;
-        var board = try Board.init(alloc, args);
+    var board = try Board.init(alloc, args);
 
-        const command = board.commands.get(args.command);
-        if (command) |cmd| {
-            try argparse.fillCommandArgs(&cmd, &args, .{ .other_flags = global_flags, .other_options = global_options });
-            try args.printAllDebug();
-            try cmd.action(&board, args);
-        } else {
-            std.log.err("Invalid Command! Rerun with help for a list of commands", .{});
-        }
+    const command = board.commands.get(args.command);
+    if (command) |cmd| {
+        try argparse.fillCommandArgs(&cmd, &args, .{ .other_flags = global_flags, .other_options = global_options });
+        try args.printAllDebug();
+        try cmd.action(&board, args);
+    } else {
+        std.log.err("Invalid Command! Rerun with help for a list of commands", .{});
     }
 }
